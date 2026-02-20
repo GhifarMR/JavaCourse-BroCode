3e6785d507430efa406a38c49c92982aab8d7657
Ghifar Maulana Ristyono <tyrtwqptllpck@gmail.com>
2026-02-20 08:13:58 +0700

    first commit

---
+++ ShoppingCartProgram/src/shoppingcartprogram/ShoppingCartProgram.java
@@ -0,0 +1,68 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package shoppingcartprogram;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author macbookair
+ */
+public class ShoppingCartProgram {
+
+    /**
+     * @param args the command line arguments
+     */
+    
+    public static String productName, currency = "$";
+    public static Double productPrice, total;
+    public static int productQuantity;
+    
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        
+        
+        Scanner scanner = new Scanner(System.in);
+        
+        System.out.print("What item would you like to buy? : ");
+        productName = scanner.nextLine();
+        
+        System.out.print("What is the price for each? : ");
+        productPrice = scanner.nextDouble();
+        scanner.nextLine();
+        
+        System.out.print("How many you like? : ");
+        productQuantity = scanner.nextInt();
+        scanner.nextLine();
+            
+        while(productQuantity < 1) {
+            System.out.println("Minimum order 1");
+            System.out.print("How many you like? : ");
+            productQuantity = scanner.nextInt();
+            scanner.nextLine();
+        }
+        
+        generateReceipt();
+        
+    }
+    
+    public static void generateReceipt() {
+        total = productPrice * productQuantity;
+        
+        System.out.println("\n----------Receipt----------");
+        
+        if(productQuantity > 1) {
+            System.out.println("You have bought " + productQuantity + " " + productName + "'s");
+        } else {
+            System.out.println("You have bought " + productQuantity + " " + productName);
+        }
+        
+        System.out.println("Your total is " + currency + total);
+        
+        System.out.println("---------------------------");
+    }
+    
+}
